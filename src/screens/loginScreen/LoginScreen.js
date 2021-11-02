0e71d603ebd0d0c2e710eb60b8eb80027bf9b36c
Ridho Tamma <58821061+ridhotamma@users.noreply.github.com>
2021-11-02 19:38:36 +0700

    change the login interface

--- src/screens/loginScreen/LoginScreen.js
+++ src/screens/loginScreen/LoginScreen.js
@@ -1,40 +1,44 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useHistory } from 'react-router-dom'
-import { login } from '../../redux/actions/auth.action'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { login } from "../../redux/actions/auth.action";
+import useWindowDimensions from "../../WindowScreen";
 
-import './loginScreen.scss'
+import "./loginScreen.scss";
 
 const LoginScreen = () => {
-   const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-   const accessToken = useSelector(state => state.auth.accessToken)
+  const accessToken = useSelector((state) => state.auth.accessToken);
+  const { height, width } = useWindowDimensions();
+  const handleLogin = () => {
+    dispatch(login());
+  };
 
-   const handleLogin = () => {
-      dispatch(login())
-   }
+  const history = useHistory();
 
-   const history = useHistory()
+  useEffect(() => {
+    if (accessToken) {
+      history.push("/");
+    }
+  }, [accessToken, history]);
 
-   useEffect(() => {
-      if (accessToken) {
-         history.push('/')
-      }
-   }, [accessToken, history])
-
-   return (
-      <div className='login'>
-         <div className='login__container'>
-            <h2>Youtube Clone</h2>
-            <img
-               src='http://pngimg.com/uploads/youtube/youtube_PNG2.png'
-               alt=''
-            />
-            <button onClick={handleLogin}>Login With google</button>
-            <p>This Project is made using YOUTUBE DATA API</p>
-         </div>
+  return (
+    <div className="login">
+      {width < 780 ? null : <div className="leftside"></div>}
+      <div className="rightside">
+        <div className="rightside__text-wrapper">
+          <img
+            src="https://download.logo.wine/logo/YouTube/YouTube-Logo.wine.png"
+            width="300"
+          />
+          <h1>Welcome to youtube clone</h1>
+          <p>you can login using your google account</p>
+          <button onClick={handleLogin}>Log In</button>
+        </div>
       </div>
-   )
-}
+    </div>
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
